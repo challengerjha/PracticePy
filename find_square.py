08b42a282ab0d4501cb173f3f8738c667e164781
challengerjha <challengerwinjhaofficial@gmail.com>
2025-12-31 18:27:24 +0530

    find the square of a given number by user

---
+++ find_square.py
@@ -0,0 +1,12 @@
+# Write a python program to find the square of a given number by user.
+
+# Taking input from the user
+
+num = int(input("Enter the Desired Number: "))
+
+# Calculating the square of the numbers
+
+square_num = num * num
+
+# Printing the square of the number
+print("The Square of the Given Number is: ", square_num)
