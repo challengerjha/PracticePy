4b14d32b5cae349f8d87a4171b68ca54103f60fb
challengerjha <challengerwinjhaofficial@gmail.com>
2025-12-31 17:08:57 +0530

    Python program to add two numbers provided by the user

---
+++ addnumbers.py
@@ -0,0 +1,10 @@
+# Python program to add two numbers provided by the user
+
+First_num = int(input("Enter first number: "))
+Second_num = int(input("Enter second number: "))
+
+# Adding two numbers
+Sum = First_num + Second_num
+
+# Print the result
+print("The sum of First Number and Second Number is: ", Sum)
