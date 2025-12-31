2840a804dc7aa2fdcc0e5396deee2abacf709cd0
challengerjha <challengerwinjhaofficial@gmail.com>
2025-12-31 18:18:47 +0530

    find the average of two numbers

---
+++ find_average.py
@@ -0,0 +1,13 @@
+# Write a Python program to find the average of two numbers entered by the user.
+
+
+# Taking input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculating the average
+
+average = (num1 + num2) / 2
+
+# printing the result 
+print("The average of ", num1, "and", num2, "is: ", average)
