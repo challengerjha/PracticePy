801673d7722c005332fb351bdba18241c8a1186d
challengerjha <challengerwinjhaofficial@gmail.com>
2025-12-31 17:46:04 +0530

    Use comparision operators to find out whether a given variable a is greater than b

---
+++ comparison_operator.py
@@ -0,0 +1,11 @@
+# Use comparision operators to find out whether a given variable a is greater than b or not. Take a = 34 and b = 80.
+
+# Assigning values to variables
+a = 34
+b = 80
+
+# Using comparison operator to check if a is greater than b
+Check = a < b
+
+# Printing the result
+print("Is a greater than b> ", Check)
