ed352162653e3f7af8421bc7940fcd5741a8c625
challengerjha <challengerwinjhaofficial@gmail.com>
2025-12-31 17:26:55 +0530

    check type of variable assigned

---
+++ checktypeofvar.py
@@ -0,0 +1,13 @@
+# Write a python program to check type of variable assigned using input function.
+
+# Taking input from user
+
+user_input = input("Enter Something to like: ")
+
+# Checking type of variable
+
+variable_type = type(user_input)
+
+# Printing the type of variable
+
+print("The type of the variable is: ", variable_type)
