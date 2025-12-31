428e43892e56f0ca387f0d38b72e829c32f5ec03
challengerjha <challengerwinjhaofficial@gmail.com>
2025-12-31 17:16:36 +0530

    python program to find the remainder

---
+++ findremainder.py
@@ -0,0 +1,10 @@
+# Write a python program to find the remainder when one number is divided by Z
+
+First_num = int(input("Enter first number: "))
+Second_num = int(input("Enter second number: "))
+
+# Finding Remainder 
+Remainder = First_num % Second_num
+
+# Print the remainder
+print("The remainder when First number is divided by Second number is: ", Remainder)
