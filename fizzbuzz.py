1e0fd71aeb016286ecb497dbbb17afed46abb30b
challengerjha <challengerwinjhaofficial@gmail.com>
2026-01-03 15:17:32 +0530

    Added FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+# Print FizzBuzz (Classic Amazon Question)
+
+# Problem
+# Print numbers from 1 to n:
+
+# "Fizz" if divisible by 3
+
+# "Buzz" if divisible by 5
+
+# "FizzBuzz" if divisible by both
+
